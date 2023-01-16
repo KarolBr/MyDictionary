42b977b9603c686fcb56a5d359b2f638c5f6231b
Karol Briegmann <kbriegmann@wp.pl>
2023-01-16 19:14:37 +0100

    Create new class Dictionary

---
+++ include/Dictionary.h
@@ -0,0 +1,20 @@
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <vector>
+
+class Dictionary
+{
+public:
+    Dictionary(){}
+
+private:
+
+
+
+};
+
+
+#endif // Dictionary.h
