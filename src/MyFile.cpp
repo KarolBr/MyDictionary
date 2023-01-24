88978c5b201b3780c198afd0302b246b780232ec
Karol Briegmann <kbriegmann@wp.pl>
2023-01-24 21:08:05 +0100

    Add new class MyFile

---
+++ src/MyFile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include "MyFile.h"
+#include "Dictionary.h"
+
+
+void MyFile::saveFile(Dictionary dict)
+{
+
+
+}
+
+
+void MyFile::loadFile(Dictionary dict)
+{
+
+}
